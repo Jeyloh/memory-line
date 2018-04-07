0c3d4323f3e2f4beeb6253fe9770494aa756acd2
Lybeck Hansen <jorgen.lybeck-hansen@capgemini.com>
2018-04-08 01:50:33 +0200

    gitignore

---
+++ src/firebase/credentials/client.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "apiKey": process.env.REACT_APP_API_KEY,
+  "clientId": process.env.REACT_APP_CLIENT_ID,
+  "authDomain":process.env.REACT_APP_AUTH_DOMAIN,
+  "databaseURL": process.env.REACT_APP_DATABASE_URL,
+  "projectId":  process.env.REACT_APP_PROJECT_ID,
+  "storageBucket": process.env.REACT_APP_STORAGE_BUCKET,
+  "messagingSenderId": process.env.REACT_APP_MESSAGE_SENDER_ID
+}
